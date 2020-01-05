4624426cd5faea674dc1ee26bca3d472a584400f
Arif <ayon.arif.10@gmail.com>
2020-01-05 19:06:22 +0600

    first commit

---
+++ SoilEvaluation/src/Main.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Asif
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        NewJFrame frame = new NewJFrame();
+        frame.setTitle("Soil Evaluation Tool");
+        frame.setResizable(false);
+        frame.setVisible(true);
+    }
+    
+}
